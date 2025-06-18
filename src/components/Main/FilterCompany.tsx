6725a7805583d020d0ce5eff569ab28c29fbd8ed
Liliia Galkina <vvlili@yandex.ru>
2025-06-18 23:16:47 +0300

    сверстаны блоки фильтров без адаптива с кастомными чекбоксами

--- src/components/Main/FilterCompany.tsx
+++ src/components/Main/FilterCompany.tsx
@@ -1,33 +1,79 @@
 import style from "./main.module.scss";
+import { useState } from "react";
 
 export default function FilterCompany() {
-	return (
+  const [filterCompany1, setFilterCompany1] = useState(false);
+  const [filterCompany2, setFilterCompany2] = useState(false);
+  const [filterCompany3, setFilterCompany3] = useState(false);
+
+  function handleFilterCompanyClick1() {
+    setFilterCompany1(!filterCompany1);
+  }
+
+  function handleFilterCompanyClick2() {
+    setFilterCompany2(!filterCompany2);
+  }
+
+  function handleFilterCompanyClick3() {
+    setFilterCompany3(!filterCompany3);
+  }
+  return (
     <div className={style.filter}>
       <h2 className={style.filter__title}>Компании</h2>
       <div className={style.filter__items}>
         <div className={style.filter__row}>
           <input
-            type="radio"
-            name="company"
-            className={style.filter__rowradio}
+            type="checkbox"
+            id="company1"
+            className={
+              filterCompany1
+                ? style.filter__rowcheckboxcircle +
+                  " " +
+                  style.filter__rowcheckboxcircle_after
+                : style.filter__rowcheckboxcircle
+            }
+            checked={filterCompany1}
+            onChange={handleFilterCompanyClick1}
           />
-          <p className={style.filter__rowtext}>Победа</p>
+          <label htmlFor="company1" className={style.filter__rowtext}>
+            Победа
+          </label>
         </div>
         <div className={style.filter__row}>
           <input
-            type="radio"
-            name="company"
-            className={style.filter__rowradio}
+            type="checkbox"
+            id="company2"
+            className={
+              filterCompany2
+                ? style.filter__rowcheckboxcircle +
+                  " " +
+                  style.filter__rowcheckboxcircle_after
+                : style.filter__rowcheckboxcircle
+            }
+            checked={filterCompany2}
+            onChange={handleFilterCompanyClick2}
           />
-          <p className={style.filter__rowtext}>Red Wings</p>
+          <label htmlFor="company2" className={style.filter__rowtext}>
+            Red Wings
+          </label>
         </div>
         <div className={style.filter__row}>
           <input
-            type="radio"
-            name="company"
-            className={style.filter__rowradio}
+            type="checkbox"
+            id="company3"
+            className={
+              filterCompany3
+                ? style.filter__rowcheckboxcircle +
+                  " " +
+                  style.filter__rowcheckboxcircle_after
+                : style.filter__rowcheckboxcircle
+            }
+            checked={filterCompany3}
+            onChange={handleFilterCompanyClick3}
           />
-          <p className={style.filter__rowtext}>S7 Airlines</p>
+          <label htmlFor="transit3" className={style.filter__rowtext}>
+            3 пересадки
+          </label>
         </div>
       </div>
     </div>
--- src/components/Main/FilterCountTransfers.tsx
+++ src/components/Main/FilterCountTransfers.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import style from "./main.module.scss";
+import mark from "../../assets/mark.svg"
 
 export default function FilterCountTransfers() {
-	return (
+  const [filterTransit0, setFilterTransit0] = useState(false);
+  const [filterTransit1, setFilterTransit1] = useState(false);
+  const [filterTransit2, setFilterTransit2] = useState(false);
+	const [filterTransit3, setFilterTransit3] = useState(false);
+	
+	function handleFilterTransitClick0() {
+		setFilterTransit0(!filterTransit0);
+	}
+
+		function handleFilterTransitClick1() {
+      setFilterTransit1(!filterTransit1);
+	}
+	
+		function handleFilterTransitClick2() {
+      setFilterTransit2(!filterTransit2);
+	}
+	
+		function handleFilterTransitClick3() {
+      setFilterTransit3(!filterTransit3);
+	}
+	
+  return (
     <div className={style.filter}>
       <h2 className={style.filter__title}>Количество пересадок</h2>
       <div className={style.filter__items}>
@@ -10,32 +33,60 @@ export default function FilterCountTransfers() {
             type="checkbox"
             id="transit0"
             className={style.filter__rowcheckbox}
+            checked={filterTransit0}
+            onChange={handleFilterTransitClick0}
+            style={{
+              backgroundImage: filterTransit0 ? `url(${mark})` : "none",
+            }}
           />
-          <p className={style.filter__rowtext}>Без пересадок</p>
+          <label htmlFor="transit0" className={style.filter__rowtext}>
+            Без пересадок
+          </label>
         </div>
         <div className={style.filter__row}>
           <input
             type="checkbox"
             id="transit1"
             className={style.filter__rowcheckbox}
+            checked={filterTransit1}
+            onChange={handleFilterTransitClick1}
+            style={{
+              backgroundImage: filterTransit1 ? `url(${mark})` : "none",
+            }}
           />
-          <p className={style.filter__rowtext}>1 пересадка</p>
+          <label htmlFor="transit1" className={style.filter__rowtext}>
+            1 пересадка
+          </label>
         </div>
         <div className={style.filter__row}>
           <input
             type="checkbox"
             id="transit2"
             className={style.filter__rowcheckbox}
+            checked={filterTransit2}
+            onChange={handleFilterTransitClick2}
+            style={{
+              backgroundImage: filterTransit2 ? `url(${mark})` : "none",
+            }}
           />
-          <p className={style.filter__rowtext}>2 пересадки</p>
+          <label htmlFor="transit2" className={style.filter__rowtext}>
+            2 пересадки
+          </label>
         </div>
         <div className={style.filter__row}>
           <input
             type="checkbox"
             id="transit3"
             className={style.filter__rowcheckbox}
+            checked={filterTransit3}
+            onChange={handleFilterTransitClick3}
+            style={{
+              backgroundImage: filterTransit3 ? `url(${mark})` : "none",
+            }}
           />
-          <p className={style.filter__rowtext}>3 пересадки</p>
+          <label htmlFor="transit3" className={style.filter__rowtext}>
+            3 пересадки
+          </label>
         </div>
       </div>
     </div>
